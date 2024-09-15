bbf798943daf01ede904e9ff438cbc599f2cedb3
alibhtty <77213825+alibhtty@users.noreply.github.com>
2024-09-15 08:03:36 +0200

    1.0.0

--- src/data.js
+++
@@ -1,5 +0,0 @@
-export const colors = [0xf74a8a, 0x111111, 0xf74a8a, 0x362f31];
-
-// changarrito colors   export const colors = [0xf1ff54, 0xf74a8a, 0xff3b44, 0x2e8cff];
-
-//export const colors = [0x8c75ff, 0x5cffab, 0xf74a8a, 0x3df2f2];

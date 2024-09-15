b83650ce7e1f0969914060d620b0d27c8ade974a
alibhtty <77213825+alibhtty@users.noreply.github.com>
2024-09-15 08:00:57 +0200

    Initial commit

---
+++ src/CodropsFrame.jsx
@@ -0,0 +1,43 @@
+const CodropsFrame = () => {
+  return (
+    <div className="frame">
+      <header className="frame__header">
+        {/* <h1 className="frame__title">
+          Model Texture Transition And Procedural Radial Noise Using <a target="_blank" href="https://tympanus.net/codrops/demos/?tag=webgl">WebGL</a>
+        </h1>
+        <a
+          aria-label="Read Article"
+          className="frame__back"
+          href="https://tympanus.net/codrops/?p=77050"
+          target="_blank"
+        >
+          Article
+        </a>
+        <a
+          className="frame__prev" 
+          target="_blank" 
+          href="https://tympanus.net/codrops/demos/"
+        >
+          All demos
+        </a>
+        <a
+          className="frame__github"
+          target="_blank" 
+          href="https://github.com/mohAmineBrs/codrops-noise-transition/"
+        >
+          Github
+        </a> */}
+        <a
+          className="frame__changarrito"
+          target="_blank" 
+          href="https://changarrito.es"
+        >
+          el changarrito
+        </a>
+      </header>
+      <button className="click__anywhere">Disponible en nuestros restaurantes desde el 04 Diciembre</button>
+    </div>
+  );
+};
+
+export default CodropsFrame;
